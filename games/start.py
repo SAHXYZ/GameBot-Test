88d725d8029a4733ad608dec23e18e14f9d206ee
SAHXYZ <opsahalam97@gmail.com>
2025-11-18 03:47:58 +0530

    Update start.py

--- games/start.py
+++ games/start.py
@@ -45,20 +45,20 @@ def init_help(bot: Client):
     @bot.on_message(filters.command("help"))
     async def help_cmd(_, msg):
         text = (
-            "🎮 **GameBot Commands**\n\n"
-            "**General Commands**\n"
-            "/start - Show menu\n"
-            "/help - Show this help menu\n"
-            "/profile - Your stats\n"
-            "/leaderboard - Top players\n\n"
-            "**Game Commands**\n"
-            "/flip - Coin flip game\n"
-            "/roll - Random dice roll\n"
-            "/fight - Fight another user\n"
-            "/rob - Rob a user\n"
-            "/guess - Word guessing game\n"
-            "/work - Earn coins by working\n"
-            "/shop - Purchase items\n"
-        )
+    "🎮 **GameBot Commands**\n\n"
+    "**General Commands**\n"
+    "/start - Show menu\n"
+    "/help - Show this help menu\n"
+    "/profile - Your stats\n"
+    "/leaderboard - Top players\n\n"
+    "**Game Commands**\n"
+    "/flip - Coin flip game\n"
+    "/roll - Random dice roll\n"
+    "/fight - Fight another user\n"
+    "/rob - Rob a user\n"
+    "/guess - Word guessing game\n"
+    "/work - Earn coins by working\n"
+    "/shop - Purchase items\n"
+)
 
         await msg.reply(text)
