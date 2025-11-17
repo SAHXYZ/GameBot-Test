05126aa599b4b47583876ae3a95adcead256538a
SAHXYZ <opsahalam97@gmail.com>
2025-11-18 03:44:39 +0530

    Update start.py

--- games/start.py
+++ games/start.py
@@ -35,3 +35,30 @@ def init_start(bot: Client):
         )
 
         await msg.reply(text, reply_markup=buttons)
+
+# filename: games/help.py
+
+from pyrogram import Client, filters
+
+def init_help(bot: Client):
+
+    @bot.on_message(filters.command("help"))
+    async def help_cmd(_, msg):
+        text = (
+            "🎮 **GameBot Commands**\n\n"
+            "**General Commands**\n"
+            "/start - Show menu\n"
+            "/help - Show this help menu\n"
+            "/profile - Your stats\n"
+            "/leaderboard - Top players\n\n"
+            "**Game Commands**\n"
+            "/flip - Coin flip game\n"
+            "/roll - Random dice roll\n"
+            "/fight - Fight another user\n"
+            "/rob - Rob a user\n"
+            "/guess - Word guessing game\n"
+            "/work - Earn coins by working\n"
+            "/shop - Purchase items\n"
+        )
+
+        await msg.reply(text)
