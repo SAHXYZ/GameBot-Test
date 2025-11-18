c4653c983f6b7155dcf08f04bf91b20191df8851
SAHXYZ <opsahalam97@gmail.com>
2025-11-18 19:11:31 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,3 +3,7 @@ import os
 API_ID = int(os.getenv("API_ID"))
 API_HASH = os.getenv("API_HASH")
 BOT_TOKEN = os.getenv("BOT_TOKEN")
+
+# MongoDB URI from Heroku Config Vars
+MONGO_URI = os.getenv("MONGO_URI")   # example: mongodb+srv://user:pass@cluster.mongodb.net
+DB_NAME = os.getenv("DB_NAME", "GameBot")  # default = GameBot
